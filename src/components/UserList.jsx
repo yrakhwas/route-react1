f4633c52fda75a1b50d9c4c05fce06a48123a97f
yrakhwas <yrakhwas@gmail.com>
2025-07-09 15:08:34 +0300

    Add User List

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,10 +1,59 @@
+import { Button } from "antd";
+import { useEffect, useState } from "react";
 import User from "./User";
 
+const url = "https://dummyjson.com/users";
 
+const pagination = {
+        skip: 0,
+        limit: 15
+    }
 function UserList()
 {
+
+    
+
+    //let users = [];
+    const [users, setUsers] = useState([]);
+
+
+    useEffect(()=>{
+        //console.log("useEffect called");
+        loadUsers();
+        // return()=>{
+        //     console.log("Cleanup function called");
+        // }
+    }, [])
+    // console.log( "Before updating data" , users); 
+    const loadUsers = () =>{
+        let apiUrl = `${url}?limit=${pagination.limit}&skip=${pagination.skip}`;
+        pagination.skip += pagination.limit;
+
+        fetch(apiUrl)
+        .then(response=>response.json())
+        .then(data => {
+            setUsers([...users,...data.users]); 
+            // console.log( "Inside function updating data" , users); 
+            
+        }
+    )
+    }
+    // console.log( "After updating data" , users); 
+    // loadUsers();
     return(
-        <User/>
+       <>
+        <h2>Users list</h2>
+        <Button onClick={loadUsers}>Load</Button>
+        <ol>
+            {
+                users.map(u=>
+                    <li key={u.id}>User : {u.firstName} {u.lastName} Age : {u.age}</li>
+                    //<User {...u}/>
+                )
+            }
+        </ol>
+        <Button onClick={loadUsers}>More...</Button>
+       </>
     )
 }
 
